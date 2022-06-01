116f281722e359c68d7b8f0d95e73b9efd6b85b7
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-01 17:19:39 +0530

    Time: 18 ms (62.39%), Space: 13.2 MB (82.44%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    void make_zeroes(vector<vector<int>>& matrix, int i, int j, int m, int n) {
-        for(int x = i; x >= 0; x--) {
-            matrix[x][j] = 0;
-        }
-        
-        for(int x = i; x < m; x++) {
-            matrix[x][j] = 0;
-        }
-        
-        for(int x = j; x >= 0; x--) {
-            matrix[i][x] = 0;
-        }
-        
-        for(int x = j; x < n; x++) {
-            matrix[i][x] = 0;
-        }
-    }
     
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        bool col0 = true;
         
-        //Approach using additional space that keeps track of indices that have a zero
-        vector<vector<int>> indices;
-        
-        for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[i].size(); j++) {
-                if(matrix[i][j] == 0)
-                    indices.push_back({i, j});
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) col0 = false;
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
             }
         }
         
-        for(int i = 0; i < indices.size(); i++) {
-            make_zeroes(matrix, indices[i][0], indices[i][1], m, n);
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 1; j--) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if(!col0) matrix[i][0] = 0;
+            
         }
     }
 };
