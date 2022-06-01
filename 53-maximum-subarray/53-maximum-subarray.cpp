f7389cd1ca425313c8cf6db50758b779cdbbf590
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-01 22:34:36 +0530

    Time: 142 ms (53.80%), Space: 67.8 MB (54.77%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
+    
+    //Kadane's Algorithm
+    
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int sum = 0;
         
-        for(int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if(sum > maxSum) maxSum = sum;
-            if(sum < 0) sum = 0;
+        int currSum = nums[0], maxSum = nums[0];
+        
+        //taken first element assuming size of current subarray to be 1
+        for(int i = 1; i < nums.size(); i++) {
+            
+            //check whether adding the current element adds any value to the subarray being carried so far
+            //and also check whether current element itself is bigger than currSum
+            
+            //basically seeing whether we should continue with previous subarray or move forward with the
+            //element
+            currSum = max(currSum + nums[i], nums[i]);
+            
+            //now after getting the optimal curr sum compare it with the maximum sum we have encountered
+            //so far
+            maxSum = max(currSum, maxSum);
         }
+        
         
         return maxSum;
     }
