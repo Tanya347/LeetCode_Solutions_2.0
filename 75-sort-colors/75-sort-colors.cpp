b7b57b0ddfef246f02242f9a6f1f82881c423a5d
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-01 21:50:32 +0530

    Time: 0 ms (100.00%), Space: 8 MB (99.53%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> count;
-        for(int i = 0; i < nums.size(); i++) {
-            count[nums[i]]++;
-        }
+        //i is just the iterator, j is used to maintain position of 1s encountered while iterating meanwhile k
+        //is used to maintain the index from end
+        int i = 0, j = 0, k = nums.size() - 1;
         
-        int index = 0;
-        for(int i = 0; i < nums.size(); ) {
-            int j = i;
-            int c = count[index];
-            while(c > 0) {
-                nums[j++] = index;
-                c--;
+        //using two pointers travel from beginning and end both
+        while(i <= k) {
+            
+            //if 0 swap with jth element which could be 1
+            if(nums[i] == 0) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+                
+            //if 1 just move on, notice here j will remain same hence retaining the postion of j
+            } else if(nums[i] == 1) {
+                i++;
+                
+            //if 2 then swap with kth element in an attempt to send it to the back
+            } else if(nums[i] == 2) {
+                swap(nums[i], nums[k]);
+                k--;
             }
-            i = i + count[index];
-            index++;
         }
     }
 };
+
