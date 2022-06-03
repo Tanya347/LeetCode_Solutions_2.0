91944929f10fb2f3d19a9a5e5cb70c79bfcdfa0b
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-03 12:20:33 +0530

    Time: 130 ms (52.06%), Space: 21.1 MB (45.70%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,37 +2,35 @@ class Solution {
 public:
     //-4 -1 -1 0 1 2
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        if(n < 3)
+            return ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> result;
         
-        if(nums.size() < 3)
-            return result;
-        
-        for(int i = 0; i < nums.size() - 2;) {
+        for(int k = 0; k < n - 2; k++) {
+            if(k != 0 && nums[k] == nums[k - 1])
+                continue;
+            int x = 0 - nums[k];
+            int i = k + 1, j = n - 1;
             
-            int j = i + 1, k = nums.size() - 1;
-            int x = 0 - nums[i];
-            
-            while(j < k) {
-                if(nums[j] + nums[k] == x) {
-                    result.push_back({nums[i], nums[j], nums[k]});
-                    while(j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
-                    j++;
-                    while(k - 1 >= 0 && nums[k] == nums[k - 1]) k--;
-                    k--;
-                } else if(nums[j] + nums[k] > x) {
-                    while(k - 1 >= 0 && nums[k] == nums[k - 1]) k--;
-                    k--;
-                } else {
-                    while(j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
-                    j++;
-                }
+            while(i < j) {
+                    if(nums[i] + nums[j] == x) {
+                        vector<int> a = {nums[i], nums[j], nums[k]};
+                        ans.push_back(a);
+                        int im = i, jn = j;
+                        while(i < nums.size() && nums[i] == nums[im])
+                            i++;
+                        while(j < nums.size() && nums[j] == nums[jn])
+                            j--;
+                    }
+                else if(nums[i] + nums[j] < x)
+                    i++;
+                else
+                    j--;
             }
-            
-            while(i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
-                i++;
         }
         
-        return result;
+        return ans;
     }
 };
