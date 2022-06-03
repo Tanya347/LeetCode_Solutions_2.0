e858e3ebe534d6329992d9b278546a6eedf62952
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-03 20:12:33 +0530

    Time: 941 ms (7.64%), Space: 147.8 MB (92.98%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -5,18 +5,22 @@ public:
     vector<vector<int>> sum;
     NumMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        sum = vector<vector<int>>(m + 1, vector<int>(n + 1)); // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+        sum = vector<vector<int>>(m, vector<int>(n + 1)); 
+        //accumulate sum in rows
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sum[i][j + 1] = sum[i][j] + matrix[i][j];
             }
         }
     }
     int sumRegion(int r1, int c1, int r2, int c2) {
-		// Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
+		// subtract the sum of prior columns from the onces required
       
-        r1++; c1++; r2++; c2++; 
-        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
+        int s = 0;
+        for(int r = r1; r <= r2; r++) {
+            s += sum[r][c2 + 1] - sum[r][c1];
+        }
+        return s;
     }
 };
 
