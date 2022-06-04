8250b7aab8e5c8a2f63f76d154899ab93f35b6fa
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-04 23:13:55 +0530

    Time: 3 ms (68.71%), Space: 7.3 MB (53.70%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     
-    void subsets_helper(vector<vector<int>>& ans, vector<int>& subset, vector<int>nums, int n) {
+    void subsets_helper(vector<int>&nums,int n,vector<vector<int>>&ans)
+    {
         if(n == 0) {
-            ans.push_back(subset);
+            ans.push_back({});
             return;
-        }    
+        }
+
+        subsets_helper(nums, n - 1, ans);
         
-        subset.push_back(nums[n - 1]);
-        subsets_helper(ans, subset, nums, n - 1);
+        int count = ans.size();
         
-        subset.pop_back();
-        subsets_helper(ans, subset, nums, n - 1);
-        
+        for(int i = 0 ; i < count; i++) {
+            ans.push_back(ans[i]);
+            ans[i].push_back(nums[n - 1]);
+        }
+        return;
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        subsets_helper(nums,nums.size(),ans);
+        return ans;    
     }
     
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> subset;
-        subsets_helper(ans, subset, nums, nums.size());
-        return ans;
-    }
+//     void subsets_helper(vector<vector<int>>& ans, vector<int>nums, int n) {
+//         if(n == nums.size()) {
+//             ans.push_back({});
+//             return;
+//         }    
+        
+//         subsets_helper(ans, nums, n + 1);
+        
+//         for(int i = 0; i < ans.size(); i++) {
+//             ans.push_back(ans[i]);
+//             ans[i].push_back(nums[n]);
+//         }
+//     }
+    
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         subsets_helper(ans, nums, 0);
+//         return ans;
+//     }
 };
