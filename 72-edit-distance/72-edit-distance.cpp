a9287198ed79c7583829fc7e4760a7b3d0e92e66
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-09 16:41:37 +0530

    Time: 19 ms (54.33%), Space: 9.1 MB (37.55%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int minDistance(string s1, string s2, vector<vector<int>>& dp, int i, int j) {
-        
-        if(s1.size() == 0 || s2.size() == 0) {
-            return max(s1.size(), s2.size());
-        }
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if (s1[0] == s2[0]) 
-            dp[i][j] = minDistance(s1.substr(1), s2.substr(1), dp, i + 1, j + 1); 
-        
-        else {
-            int x = 1 + minDistance(s1.substr(1), s2, dp, i + 1, j);
-            int y = 1 + minDistance(s1,s2.substr(1), dp, i, j + 1);
-            int z = 1 + minDistance(s1.substr(1), s2.substr(1), dp, i + 1, j + 1);
-        
-            dp[i][j] = min(x , min(y , z));
-        }
-        
-        return dp[i][j];
-    }
-    
     int minDistance(string s1, string s2) {
         
-        vector<vector<int>> dp(s1.length() + 1, vector<int>(s2.length() + 1, -1));
+        int m = s1.size();
+        int n = s2.size();
+        vector<vector<int>> output(m + 1, vector<int>(n + 1));
+    
+        for(int i = 0; i <= m; i++) {
+
+            for(int j = 0; j <= n; j++) {
+                
+                if(i == 0)
+                    output[i][j] = j;
+                
+                else if(j == 0)
+                    output[i][j] = i;
+                
+                else {
+                    if(s1[m - i] == s2[n - j])
+                        output[i][j] = output[i - 1][j - 1];
+                    
+                    else
+                    output[i][j] = 1 + min(min(output[i-1][j], output[i][j-1]), output[i-1][j-1]);
+                }
+            }
+        }
         
-        return minDistance(s1, s2, dp, 0, 0);
+        return output[m][n];
     }
 };
