098d9c91451e3a117700bc55cf5a5b469c087499
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-09 11:48:29 +0530

    Update CN_dp.cpp

--- 64-minimum-path-sum/CN_dp.cpp
+++ 64-minimum-path-sum/CN_dp.cpp
@@ -1,1 +1,29 @@
-
+int minCostPath(int **input, int m, int n)
+{
+	//Write your code here
+    
+    int **dp = new int*[m];
+    for(int i = 0; i < m; i++) {
+        dp[i] = new int[n];
+    }
+    
+    dp[m - 1][n - 1] = input[m - 1][n - 1];
+    
+    for(int i = m - 2; i >= 0; i--) {
+        dp[i][n - 1] = input[i][n - 1] + dp[i + 1][n - 1];
+    }
+    
+    for(int i = n - 2; i >= 0; i--) {
+        dp[m - 1][i] = input[m - 1][i] + dp[m - 1][i + 1];
+    }
+    
+    for(int i = m - 2; i >= 0; i--) {
+        
+        for(int j = n - 2; j >= 0; j--) {
+            
+            dp[i][j] = input[i][j] + min(dp[i + 1][j], min(dp[i][j + 1], dp[i + 1][j + 1]));
+        }
+    }
+    
+    return dp[0][0];
+}
