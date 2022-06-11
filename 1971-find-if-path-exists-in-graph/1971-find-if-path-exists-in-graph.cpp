d18c42c1537bfc5f11876435927324ed075436c7
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-11 15:40:11 +0530

    Time: 719 ms (79.58%), Space: 212.5 MB (24.36%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
         
-    void dfs(vector<vector<int>>&adj,int node,vector<bool>&visit){
-            visit[node]=true;
-            for(auto child : adj[node]){
-                    if(visit[child]==false){
-                            dfs(adj,child,visit);
-                    }
+    void dfs(vector<vector<int>>& graph,int si, vector<bool>& visited){
+            
+        visited[si] = true;
+        
+        for(auto child : graph[si]){
+            if(visited[child] == false){
+                dfs(graph, child, visited);
             }
+        }
             
     }
+    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-          vector<vector<int>>adj(n);
-            for(int i=0;i<edges.size();i++){
-                    adj[edges[i][0]].push_back(edges[i][1]);
-                    adj[edges[i][1]].push_back(edges[i][0]);
+        
+          vector<vector<int>> graph(n);
+            for(int i = 0; i < edges.size(); i++){
+                    graph[edges[i][0]].push_back(edges[i][1]);
+                    graph[edges[i][1]].push_back(edges[i][0]);
             }
-            vector<bool>visit(n,false);
-            dfs(adj,source,visit);
-            return visit[destination];
+        
+            vector<bool> visited(n, false);
+        
+            dfs(graph, source, visited);
+        
+            return visited[destination];
     }
 };
