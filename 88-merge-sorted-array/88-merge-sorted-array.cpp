b7cbac19935e3cf26b87b75bd06de08b08b965ed
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-20 19:12:12 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (29.16%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
+        if(m == 0) {
+            nums1 = nums2;
+            return;
+        } else if(n == 0) {
+            return;
+        }
+        
+        
         int i = 0, j = 0;
         
-        if(n == 0)
-            return;
-        
         while(i < m && j < n) {
-            if(nums2[j] <= nums1[i]) {
+            if(nums1[i] >= nums2[j]) {
                 auto it = nums1.begin() + i;
                 nums1.insert(it, nums2[j]);
                 nums1.pop_back();
                 i++;
-                m++;
                 j++;
-            }
-            else
+                m++;
+            } 
+            else {
                 i++;
+            }
+
+                
         }
         
         int diff = n - j;
+        
         while(diff > 0) {
             nums1.pop_back();
             diff--;
@@ -29,5 +38,6 @@ public:
         while(j < n) {
             nums1.push_back(nums2[j++]);
         }
+        
     }
 };
